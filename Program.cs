2c9ed8af32f68411c4beea9a2fc374a099ba6949
MauricioZavalaAr <mauricio.zavala.ar@gmail.com>
2022-10-17 22:57:47 -0500

    change2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello!");
+
+int num1 = 1;
+int num2 = 3;
+int num3;
+num3 = num1 + num2;
+Console.WriteLine(num3);
